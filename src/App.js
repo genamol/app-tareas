f2b77be5b56118741bc1666b0666d68236ac07dd
Genaro Molina <genamolina01@gmail.com>
2024-03-25 18:22:33 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./components/Logo.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <div className="tareas-container">
+        <h1>Lista de tareas</h1>
+      </div>
     </div>
   );
 }
